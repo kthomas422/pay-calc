fbce21d6fd49736c2d6579fc86403f724d03fc0d
Kyle Thomas <mr.kyle.thomas@gmail.com>
2020-06-18 15:51:58 -0700

    added some fields

--- main.go
+++ main.go
@@ -17,11 +17,14 @@ func calculatePay(netPay float64) {
 	funTickets = netPay * .05
 	bills = netPay * .15
 
-	fmt.Printf("House Fund : %.2f\n", houseFund)
-	fmt.Printf("Debt       : %.2f\n", debt)
-	fmt.Printf("Savings    : %.2f\n", savings)
-	fmt.Printf("Fun Tickets: %.2f\n", funTickets)
-	fmt.Printf("Bills      : %.2f\n", bills)
+	fmt.Printf("House Fund : $%.2f\n", houseFund)
+	fmt.Printf("Debt       : $%.2f\n", debt)
+	fmt.Printf("Savings    : $%.2f\n", savings)
+	fmt.Printf("Fun Tickets: $%.2f\n", funTickets)
+	fmt.Printf("Bills      : $%.2f\n", bills)
+	fmt.Printf("Bill amount: $%.2f\n\n", 190.0)
+	fmt.Printf("After Bills: $%.2f\n", bills-190.0)
+	fmt.Printf("Total      : $%.2f\n", houseFund+debt+savings+funTickets+bills) // will be off due to float rounding
 }
 
 func usage(name string) {
